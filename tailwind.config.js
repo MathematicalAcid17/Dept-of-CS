370f3178a05063bebd9cb76a96c54a3e6357c509
Sanju Debnath <debnathsanju4803@gmail.com>
2022-11-30 11:16:13 +0530

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,6 @@ module.exports = {
         primary: '#044A9C',
         secondary: '#6AC9FF',
       },
-      spacing: {
-        '16' : '1025px',
-        '200' : '74px'
-      }
     },
   },
   plugins: [],
