73c68cbc19da97d7670c32e35ea9bc3d8f819f8f
Ananta Podder <ananta8967940712@gmail.com>
2022-11-30 22:26:50 +0530

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./dist/*{html,js}"],
   theme: {
     extend: {
+      fontFamily: {
+        Inter: ['Inter', 'sans-serif'],
+       },
       colors: {
         primary: '#044A9C',
         secondary: '#FFD233',
